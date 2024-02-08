30742a947d322392b2a9775ace2d6dead934afd5
AlbertTeacher <alonso.albert+profe@gmail.com>
2024-02-08 10:02:25 +0100

    first approach

---
+++ Observatori/observatori.py
@@ -0,0 +1,37 @@
+#Registre de temperatures
+temperatures = []
+
+def mostrar_menu():
+    ...
+
+def tractar_opcio():
+    ...
+
+def resgistre_temperatures_setmanas():
+    ...
+
+def mostrar_mitjana():
+    ...
+
+def mostrar_diferencia():
+    ...
+
+def finalitzar_execucio():
+    ...
+
+def llegir_temperatures_teclat():
+    lector = input("Escriu les temperatures d'aquesta setmana: ")
+    for temperatura in lector.split():
+        temperatures.append(float(temperatura.replace(',','.')))
+
+def calcula_diferencia():
+    ...
+
+def calcular_mitjana():
+    ...
+
+def mostrar_data():
+    ...
+
+def incrementar_data():
+    ...
