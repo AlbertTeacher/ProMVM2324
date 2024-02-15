265abf49977e8d5971462b807667fe602aa3aff4
AlbertTeacher <alonso.albert+profe@gmail.com>
2024-02-15 12:34:10 +0100

    Added words exercise

---
+++ PhraseWithMoreA/words_with_more_a.py
@@ -0,0 +1,31 @@
+frase_actual = ''
+frase_mes_as = ''
+max_as = 0
+fi = False
+
+def inici():
+    while not fi:
+        processar_frase()
+        mostrar_resultat_actual()
+
+def processar_frase():
+    global frase_actual, fi
+    frase_actual = input('Escriu una frase:\n')
+    if frase_actual == 'fi':
+        fi = True
+    else:
+        comparar_as()
+
+def mostrar_resultat_actual():
+    print("La frase amb mes a's es: \"", frase_mes_as, "\"", sep="")
+    print("Te", max_as, " a's.")
+
+def comparar_as():
+    global frase_mes_as, max_as
+    num_as = frase_actual.casefold().count('a')
+
+    if num_as > max_as:
+        max_as = num_as
+        frase_mes_as = frase_actual
+
+inici()
