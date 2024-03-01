48789b2f5ef6c817d4c62a06d86c689d37266dbe
AlbertTeacher <alonso.albert+profe@gmail.com>
2024-03-01 08:49:09 +0100

    Added calculadora exercise

--- Kotlin/calculadora.kt
+++ Kotlin/calculadora.kt
@@ -1,96 +1,103 @@
-fun leerOperacion(): String {
-    return "+"
+// PRE: El usuario elige que operacion quiere hacer y que valores se van a calcuar
+fun pedirOperacion(): String {
+	println("Que operacion quieres hacer? '+', '-', '*', '/':")
+	var operacion: String? = readLine()
+
+	if (operacion == null) {
+		operacion = ""
+	}
+
+	return operacion
 }
 
-fun leerNumero(): Int {
-    return 7
+fun pedirNumero(): Int {
+	print("Introduce un numero: ")
+	var numero: Int = readLine()!!.toInt()
+
+	return numero
 }
 
-fun sumar(primerNumero: Int, segundoNumero: Int): Int {
-	var resultado: Int = 0
-
-	resultado = primerNumero
+fun sumar(primerOperando: Int, segundoOperando: Int): Int {
+	var resultado: Int = primerOperando + segundoOperando
 
 	return resultado
 }
 
-/**
-fun resta(primerNumero: Int, segundoNumero: Int) {
-    val resultado: Int = primerNumero - segundoNumero
+fun resta(primerOperando: Int, segundoOperando: Int): Int {
+	var resultado: Int = primerOperando - segundoOperando
 
-    println(resultado)
+	return resultado
 }
 
-fun multiplicacion(primerNumero: Int, segundoNumero: Int) {
-    val resultado: Int = primerNumero * segundoNumero
+fun multiplicar(primerOperando: Int, segundoOperando: Int): Int {
+	var resultado: Int = primerOperando * segundoOperando
 
-    println(resultado)
+	return resultado
 }
 
-fun division(primerNumero: Int, segundoNumero: Int) {
-    var resultado: Int = 0
+fun dividir(primerOperando: Int, segundoOperando: Int): Int {
+	var resultado: Int = 0
 
-    if (segundoNumero == 0) {
-        println("No se puede dividir entre 0")
-    } else {
-        resultado = primerNumero / segundoNumero
+	if (segundoOperando == 0) {
+		println("No se puede dividir entre zero")
+	} else {
+		resultado = primerOperando / segundoOperando
+	}
 
-        println(resultado)
-    }
+	return resultado
 }
 
-fun usandoIFELSE(operacion: String, primerNumero: Int, segundoNumero: Int) {
-    println("Prueba con IF ELSE")
-    if (operacion == "+") {
-        sumar(primerNumero, segundoNumero)
-    } else if (operacion == "-") {
-        resta(primerNumero, segundoNumero)
-    } else if (operacion == "*") {
-        multiplicacion(primerNumero, segundoNumero)
-    } else if (operacion == "/") {
-        division(primerNumero, segundoNumero)
-    } else {
-        println("Invalid operacion")
-    }
+fun selecConIFELSE(operacion: String, primerOperando: Int, segundoOperando: Int): Int {
+	println("Selecciono con un IF ELSE")
+	var resultado: Int = 0
+
+	if (operacion == "+") {
+		resultado = sumar(primerOperando, segundoOperando)
+	} else if (operacion == "-") {
+		resultado = resta(primerOperando, segundoOperando)
+	} else if (operacion == "*") {
+		resultado = multiplicar(primerOperando, segundoOperando)
+	} else if (operacion == "/") {
+		resultado = dividir(primerOperando, segundoOperando)
+	} else {
+		println("Operacion no permitida")
+	}
+
+	return resultado
 }
 
-fun usandoSWITHCASE(operacion: String, primerNumero: Int, segundoNumero: Int) {
-    println("Prueba con SWITCH CASE")
-    when (operacion) {
-        "+" -> sumar(primerNumero, segundoNumero)
-        "-" -> resta(primerNumero, segundoNumero)
-        "*" -> multiplicacion(primerNumero, segundoNumero)
-        "/" -> division(primerNumero, segundoNumero)
-        else -> {
-            println("Invalid operacion")
-        }
-    }
+fun selecConSWITCH(operacion: String, primerOperando: Int, segundoOperando: Int): Int {
+	println("Selecciono con un SWITCH")
+	var resultado: Int = 0
+
+	when(operacion) {
+		"+" -> resultado = sumar(primerOperando, segundoOperando)
+		"-" -> resultado = resta(primerOperando, segundoOperando)
+		"*" -> resultado = multiplicar(primerOperando, segundoOperando)
+		"/" -> resultado = dividir(primerOperando, segundoOperando)
+	}
+
+	return resultado
 }
-*/
 
 fun main() {
-   // Debemos leer que operacion hacer
-   val operacion: String = leerOperacion()
+	/*
+	 * Primero le preguntaremos al usuario que operacion quiere hacer
+	 */
+	val operacion: String = pedirOperacion()
 
-   // Debemos leer cual es el primer numero
-   val primerNumero: Int = leerNumero()
+	// Le pediremos que nos introduzca un numero
+	val primerOperando: Int = pedirNumero()
 
-   // Debemos leer cual es el segundo numero
-   val segundoNumero: Int = leerNumero()
+	// Le pediremos que nos introduzca un segundo numero
+	val segundoOperando: Int = pedirNumero()
 
-   // Deberiamos tener un selector que escoja la operacion a invocar
-   /*
-    * Tenemos dos opciones hacer un grupo de if / else
-    * O podriamos hacer un switch
-    */
-		print("Operacion: ")
-    println(operacion)
-		print("Primer numero: ")
-    println(primerNumero)
-		print("Segundo numero: ")
-    println(segundoNumero)
-    val resultado = sumar(8, 9)
-    //usandoIFELSE(operacion, primerNumero, segundoNumero)
-    //usandoSWITHCASE(operacion, primerNumero, segundoNumero)
-	println(resultado)
+	// Escogeremos que accion debemos ejecutar segun la operacion deseada
+	val resultado1: Int = selecConIFELSE(operacion, primerOperando, segundoOperando)
+	val resultado2: Int = selecConSWITCH(operacion, primerOperando, segundoOperando)
+
+	// Imprimiremos el resultado
+	println(resultado1)
+	println(resultado2)
 }
+// POST: Retorna el resultado de la operacion realizada
