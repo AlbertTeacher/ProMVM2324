5d0b095292339727eb7dee217719eefe0c123f78
Albert <117260437+AlbertTeacher@users.noreply.github.com>
2024-04-11 21:20:59 +0200

    Update carrito.py

--- StudyExam/Ferreteria/carrito.py
+++ StudyExam/Ferreteria/carrito.py
@@ -7,12 +7,9 @@ class Carrito:
     2.4. Calcular importe
     '''
     def __init__(self):
-        self.carrito = dict()
+        self.carrito: dict = dict()
+    
     def add_product(self, product: str, qty: int) -> None:
-        '''
-        Si producto existe en el diccionario, sumar qty
-        Si no existe add
-        '''
         if product in self.carrito.keys():
             self.carrito[product] += qty
         else:
@@ -23,8 +20,25 @@ class Carrito:
             self.carrito[product] -= 1
             if self.carrit[product] <= 0:
                 self.delete_line(product)
+    
     def delete_line(self, product: str) -> None:
         if product in self.carrito.keys():
             self.carrito.delete(product)
+    
     def calculate_total(self) -> int:
-        ...
+        total: int = 0
+        for qty in self.carrito.values():
+            total += qty
+            
+        return total
+        
+    def show_lines(self) -> None:
+        line_number: int = 1
+        
+        for product, qty in self.carrito.items():
+            print(f"#{line_number}: {product} ... {qty}")
+            line_number += 1
+            
+        def pay(self) -> None:
+            print("Carrito pagat!")
+            self.carrito.clear()
